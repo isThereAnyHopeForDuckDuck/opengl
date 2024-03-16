3bb93a465d864b17619546458e286ab62782e48e
cclr <420338169@qq.com>
2024-03-16 17:00:26 +0800

    对于modelview矩阵操作，以及绘制的一些理解

--- opengl/opengl_1x/opengl_1x.cpp
+++ opengl/opengl_1x/opengl_1x.cpp
@@ -85,41 +85,102 @@ public:
         angle += 1;
         angle_moon += 7.8;
         angle_earth += 1.2;
+
+        auto drawLine = []() {
+            glBegin(GL_LINES);
+            glColor3f(1, 0, 0);
+            glVertex3f(-10, 0, 0);
+            glVertex3f(10, 0, 0);
+
+            glColor3f(0, 1, 0);
+            glVertex3f(0, -10, 0);
+            glVertex3f(0, 10, 0);
+
+            glColor3f(0, 0, 1);
+            glVertex3f(0, 0, 10);
+            glVertex3f(0, 0, -10);
+            glEnd();
+
+            glPointSize(8);
+            glColor3f(1, 1, 1);
+            glBegin(GL_POINTS);
+            glVertex3f(10, 0, 0);
+            glVertex3f(0, 10, 0);
+            glVertex3f(0, 0, 10);
+            glEnd();
+        };
+
+        auto drawRect = []() {
+            glBegin(GL_LINES);
+            glColor3f(1, 0, 0);
+            glVertex3f(-10, 10, 0);
+            glVertex3f(10, 10, 0);
+
+            glColor3f(0, 1, 0);
+            glVertex3f(-10, -10, 0);
+            glVertex3f(10, -10, 0);
+
+            glColor3f(0, 0, 1);
+            glVertex3f(-10, -10, 0);
+            glVertex3f(-10, 10, 0);
+
+            glColor3f(0, 0, 1);
+            glVertex3f(10, -10, 0);
+            glVertex3f(10, 10, 0);
+            glEnd();
+        };
+
         glMatrixMode(GL_MODELVIEW);
-
         glLoadIdentity(); 
-        /*多物体绘制原理分析
-        glTranslatef glRotatef 起始坐标系没动，是摄像机动了
-        */
-        glTranslatef(0, 0, -50);
-        glRotatef(45, 1, 0, 0);
-        glRotatef(45, 0, 0, 1);
-        glRotatef(angle, 1, 1, -1);
-        glDrawArrays(GL_QUADS, 0, 24);
-        /*
-        到这里 摄像机在X Z上都转了45度 所以方块看起来是立起来的。
-        glRotatef(angle, 1, 1, -1); 给的旋转轴  其实是相对坐标系给的。
-        glDrawArrays(GL_QUADS, 0, 24);  产生了第一张图
-        */
-        glRotatef(angle_earth, 1, 1, -1);
-        glTranslatef(10, -10, 0);
-        /*
-        因为Z旋转了45度 所以想要和第一个方块在水平线，得平移 15 -15  到4象限  严格来说这样不准确  之后再出一个详细完整版
-        产生了第2张图
-        */
-        glScalef(0.5, 0.5 , 0.5);
-        glRotatef(angle, 1, 1, -1);
-        glDrawArrays(GL_QUADS, 0, 24);
 
-        glRotatef(angle_moon, 1, 1, -1);
-        glTranslatef(5, -5, 0);
-        glRotatef(angle, 1, 1, -1);
+        // 关于平移
+        // 1. glTranslatef的三个参数，可以理解为一个向量，也可以理解为3个维度的平移量
+        //      因为直接平移到这个三维向量代表的点 和 X Y Z 分别移动，最后的效果是一样的。
+        // 2. 平移的方向 从Z轴正方向往负方向看
+        //      x+ 场景往右     意味着，摄像机往X-
+        //      y+ 场景往上     意味着，摄像机往Y-
+        //      z+ 场景靠近     意味着，摄像机往Z-
+#if 0
+        glTranslatef(1, 1, -50);
+#else
+        glTranslatef(0, 0, -50);
+#endif
+        
+        // 关于旋转
+        // 1. glRotatef的后3个参数，表示一个向量，作为旋转的中心轴。  101 和 100+001 是不同的
+        //      因为旋转是以3维向量为旋转轴，这个向量必然穿过原点，所以旋转轴一定是穿过原点的。
+        // 2. 旋转，其实不是坐标轴旋转，而是摄像机旋转 也就是GL_MODELVIEW矩阵
+        //      旋转的方向，是从旋转轴正方形往负方向看，逆时针旋转
+        // 3. 自转 自转就是旋转 只不过维护了一个角度变量，每次进来，都递增这个变量，达到自转的目的
+        //      如果这个变量一直不变，画面其实是静止的。
+        //      每次绘制，都比前一次绘制多转一点角度，从而视觉上达到自转的目的
+        // 4. 公转 
+        //      因为场景在自转，如果另外一个物体，相对于场景旋转轴上的物体位置是固定的，那么就会随着场景转，达到公转的视觉效果
+        //      为了让公转更真实，可以将物体额外做一个旋转
+#if 0
+        glRotatef(45, 1, 0, 1);
+#else
+        //glRotatef(45, 1, 0, 0);
+        glRotatef(45, 0, 0, 1);
+#endif
+        //动态的效果  其实是一张一张图叠加起来的
+        // 我先用Z轴旋转45° 画一个正方形
+        //生成第一张图
+        drawRect();
+        
+        //生成第2张图
         glScalef(0.5, 0.5, 0.5);
-        glDrawArrays(GL_QUADS, 0, 24);
-        /*
-        * 产生了第3张图
-        */
-        /*绕某个物体转，其实只要固定两个物体的位置，其中一个自转就行*/
+        glRotatef(-90, 0, 0, 1);
+        glRotatef(45, 1, 0, 0);
+        drawRect();//到这里可以看到，之前画的图并没有受影响，只是把第二张图叠上去了。
+
+        //生成第3张图
+        glLoadIdentity();//我在这清空一下
+        glTranslatef(0, 0, -50);
+        glScalef(1.5, 1.5, 1.5);
+        glRotatef(15, 0, 0, 1);
+        drawRect();//可以看到 其实是3图叠加在一起了。  可以理解为 画完一个东西后，一张图生成了，画布就清空了。
+
         glc.swapBuffer();//3图叠加
     }
 
