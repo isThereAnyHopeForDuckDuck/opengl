018796da614f6ba35cdf64287c18c362a8c31f71
cclr <420338169@qq.com>
2024-03-17 21:02:30 +0800

    添加 freeimage库  使用本地图片作为纹理数据

--- opengl/opengl_1x/opengl_1x.cpp
+++ opengl/opengl_1x/opengl_1x.cpp
@@ -12,6 +12,7 @@
 #include <math.h>
 
 #include <vector>
+#include "freeimage.h"
 
 class sampleWindow : public openglWindow {
 private:
@@ -24,7 +25,6 @@ private:
     uint32_t m_randomTextureWidth = 128, m_randomTextureHeight = 160;
 public:
     ~sampleWindow() {
-        delete[] m_textureRandom;
     }
     sampleWindow(HINSTANCE hInstance, int nCmdShow) :openglWindow(hInstance, nCmdShow) {
         HWND hWnd = m_hWnd;
@@ -51,14 +51,44 @@ public:
         glEnable(GL_TEXTURE_2D);
         m_textureId = createTexture();
 
-        //random 
-        m_textureRandom = new uint8_t[m_randomTextureWidth * m_randomTextureHeight * 3];
-
-        for (int i = 0; i < m_randomTextureWidth * m_randomTextureHeight * 3; i++) {
-            m_textureRandom[i] = i % 255;
+        textureImage("C:\\ccli\\resource\\image\\duckduck.jpeg");
+    }
+    bool textureImage(const char * pFile) {
+        const char* fileName = pFile;
+        //1 获取图片格式
+        FREE_IMAGE_FORMAT fifmt = FreeImage_GetFileType(fileName, 0);
+        if (fifmt == FIF_UNKNOWN)
+        {
+            return  false;
         }
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, m_randomTextureWidth, m_randomTextureHeight, 0, 
-            GL_RGB, GL_UNSIGNED_BYTE, m_textureRandom);
+        //2 加载图片
+        FIBITMAP* dib = FreeImage_Load(fifmt, fileName, 0);
+
+        FREE_IMAGE_COLOR_TYPE type = FreeImage_GetColorType(dib);
+
+        //! 获取数据指针
+        FIBITMAP* temp = dib;
+        dib = FreeImage_ConvertTo32Bits(dib);
+        FreeImage_Unload(temp);
+
+        BYTE* pixels = (BYTE*)FreeImage_GetBits(dib);
+        int     width = FreeImage_GetWidth(dib);
+        int     height = FreeImage_GetHeight(dib);
+
+        for (int i = 0; i < width * height * 4; i += 4)
+        {
+            BYTE temp = pixels[i];
+            pixels[i] = pixels[i + 2];
+            pixels[i + 2] = temp;
+        }
+        glBindTexture(GL_TEXTURE_2D, m_textureId);//将texID绑定  意味着之后的GL_TEXTURE_2D操作，都是对texId进行
+        // 前三个参数  纹理的类型。
+        // 后面的参数  输入图片的参数   边框参数，应该被设置为0
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 
+            width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+
+        FreeImage_Unload(dib);
+        return      true;
     }
 
     uint32_t createTexture() {
@@ -76,7 +106,7 @@ public:
     void render() override {
         glClearColor(0, 0, 0, 1);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+        
 #if 01
         glOrtho(glRect.left, glRect.right, glRect.top, glRect.bottom,  -100, 100);
         glMatrixMode(GL_MODELVIEW);
