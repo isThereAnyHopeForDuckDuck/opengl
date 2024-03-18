538e107c0cced0ec3a50529d06a360136452ede9
LiRui <lirui@malanyun.com>
2024-03-18 14:19:49 +0800

    立方体贴6张图

--- opengl/opengl_1x/opengl_1x.cpp
+++ opengl/opengl_1x/opengl_1x.cpp
@@ -20,7 +20,15 @@ private:
     RECT glRect;
     GLContext glc;
 
-    uint32_t m_textureId;
+    uint32_t m_textureId[8] = {0};
+    const uint8_t* m_textureSrc[8] = {
+        (const uint8_t*)"D:\\lr\\code\\res\\d.jpeg",
+        (const uint8_t*)"D:\\lr\\code\\res\\x.jpeg",
+        (const uint8_t*)"D:\\lr\\code\\res\\m.jpeg",
+        (const uint8_t*)"D:\\lr\\code\\res\\bat.jpeg",
+        (const uint8_t*)"D:\\lr\\code\\res\\j.jpeg",
+        (const uint8_t*)"D:\\lr\\code\\res\\L.jpeg",
+    };
     uint8_t* m_textureRandom;
     uint32_t m_randomTextureWidth = 128, m_randomTextureHeight = 160;
 public:
@@ -49,12 +57,15 @@ public:
         glEnable(GL_DEPTH_TEST);
 
         glEnable(GL_TEXTURE_2D);
-        m_textureId = createTexture();
+        
+        for (int i = 0; i < 6; i++) {
+            m_textureId[i] = createTexture();
+            textureImage(i);
+        }
 
-        textureImage("C:\\ccli\\resource\\image\\duckduck.jpeg");
     }
-    bool textureImage(const char * pFile) {
-        const char* fileName = pFile;
+    bool textureImage(int indexs) {
+        const char* fileName = (const char*)m_textureSrc[indexs];
         //1 获取图片格式
         FREE_IMAGE_FORMAT fifmt = FreeImage_GetFileType(fileName, 0);
         if (fifmt == FIF_UNKNOWN)
@@ -81,7 +92,7 @@ public:
             pixels[i] = pixels[i + 2];
             pixels[i + 2] = temp;
         }
-        glBindTexture(GL_TEXTURE_2D, m_textureId);//将texID绑定  意味着之后的GL_TEXTURE_2D操作，都是对texId进行
+        glBindTexture(GL_TEXTURE_2D, m_textureId[indexs]);//将texID绑定  意味着之后的GL_TEXTURE_2D操作，都是对texId进行
         // 前三个参数  纹理的类型。
         // 后面的参数  输入图片的参数   边框参数，应该被设置为0
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 
@@ -108,7 +119,7 @@ public:
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
 #if 01
-        glOrtho(glRect.left, glRect.right, glRect.top, glRect.bottom,  -100, 100);
+        glOrtho(glRect.left, glRect.right, glRect.top, glRect.bottom,  -200, 200);
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
         glTranslatef(glRect.right / 2, glRect.bottom / 2, 0);
@@ -122,10 +133,35 @@ public:
         };
             
         pointInfo renderPoint[] = {
-            {0, 0,      -100, -100, 0},
-            {0, 1,      -100, 100, 0},
-            {1, 1,      100, 100, 0},
-            {1, 0,      100, -100, 0},
+            {0, 0,      -100, -100, -100},
+            {0, 1,      -100, 100, -100},
+            {1, 1,      100, 100, -100},
+            {1, 0,      100, -100, -100},
+
+            {0, 0,      -100, -100, 100},
+            {0, 1,      -100, 100, 100},
+            {1, 1,      100, 100, 100},
+            {1, 0,      100, -100, 100},
+
+            {0, 0,      100 ,- 100, -100, },
+            {0, 1,      100 ,- 100, 100,  },
+            {1, 1,      100, 100, 100,   },
+            {1, 0,      100, 100, -100,  },
+
+            {0, 0,      -100 ,-100, -100, },
+            {0, 1,      -100 ,-100, 100,  },
+            {1, 1,      -100, 100, 100,   },
+            {1, 0,      -100, 100, -100,  },
+
+            {0, 0,      -100,-100 , -100, },
+            {0, 1,      -100,-100 , 100,  },
+            {1, 1,      100, -100, 100,   },
+            {1, 0,      100, -100, -100,  },
+
+            {0, 0,      -100, 100 , -100, },
+            {0, 1,      -100, 100 , 100,  },
+            {1, 1,      100,  100, 100,   },
+            {1, 0,      100,  100, -100,  },
         };
 #if 0
         glInterleavedArrays(GL_C4UB_V3F, sizeof(pointInfo), renderPoint);
@@ -136,9 +172,20 @@ public:
         glEnableClientState(GL_TEXTURE_COORD_ARRAY);
         glTexCoordPointer(2, GL_FLOAT, sizeof(pointInfo), &renderPoint[0].u);
 #endif
-        glBindTexture(GL_TEXTURE_2D, m_textureId);
+        
 
-        glDrawArrays(GL_QUADS, 0, 4);
+        static float angle = 0;
+        angle += 0.6;
+        glMatrixMode(GL_MODELVIEW);
+        glRotatef(-45, 1, 0, 0);
+        glRotatef(-45, 0, 1, 0);
+        glRotatef(angle, 1, 1, 1);
+
+        for (int i = 0; i < 6; i++) {
+            glBindTexture(GL_TEXTURE_2D, m_textureId[i]);
+            glDrawArrays(GL_QUADS, 4*i, 4);
+        }
+
         glc.swapBuffer();
     }
 
